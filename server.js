d9f81c4db51f0442a737973a4e312642614b1558
naveen1111 <ngnaven@gmail.com>
2019-12-18 20:20:33 +0530

    Database updation and routing

--- server.js
+++ server.js
@@ -2,28 +2,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+const knex = require('knex')
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'naveen',
+      password : 'naveen',
+      database : 'urlshort'
+    }
+});
 
-const database =[];
+const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
     const input_url = req.body.org_url;
-    const domain_name = 'shorty.in/';
+    const domain_name = 'http://localhost:3000/';
     const randomstring = Math.random().toString(32).substring(2,6)+Math.random().toString(32).substring(2,6);
     const output_url = domain_name+randomstring;
     
-    database.push({output_url});
-
+    db('urldb').insert({input_url:input_url,output_url:output_url})
+        .then(console.log('success'))
+        
     res.json(output_url);
+
     console.log(input_url);
     console.log(output_url);
 })
 
 app.get('/', (req,res) => {
     res.send('this is working');
+});
+
+app.get('/:code', (req,res) => {
+    res.send('this is working too');
+    console.log(req.params.code);
+    // res.redirect('https://www.youtube.com/watch?v=paNikhYqdz0');
 });
 
 app.listen(3000, () => {console.log('app is running on port 3000');
